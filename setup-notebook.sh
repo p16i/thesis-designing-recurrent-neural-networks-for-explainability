0d20701779cea99b7bc5d98ea0f3e5a2b3a1e422
heytitle <pat.chormai@gmail.com>
2017-07-31 22:55:58 +0200

    init

---
+++ setup-notebook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install extension package
+pip install jupyter_contrib_nbextensions
+
+# install js and css stuffs
+jupyter contrib nbextension install --user
+
+# enable plugins
+jupyter nbextension enable toc2/main
