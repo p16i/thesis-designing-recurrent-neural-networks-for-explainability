0ab748f3a8d282f93ae40f4ed52f8f1c9cfd1c62
heytitle <pat.chormai@gmail.com>
2017-09-18 16:55:19 +0200

    make travis work again!

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+packages = find_packages()
+print(packages)
 setup(name='Thesis',
       version='1.0',
       description='...',
       author='Pattarawat Chormai',
       author_email='pat.chormai@gmail.com',
       url='https://github.com/heytitle/thesis-designing-recurrent-neural-networks-for-explainability',
-      packages=find_packages()
+      packages=packages
       )
