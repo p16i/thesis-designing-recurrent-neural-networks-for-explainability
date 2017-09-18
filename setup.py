dc49be4e25d1d6c3b573cae69d2de881b291f50b
heytitle <pat.chormai@gmail.com>
2017-09-18 16:16:36 +0200

    make travis work

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Thesis',
+      version='1.0',
+      description='...',
+      author='Pattarawat Chormai',
+      author_email='pat.chormai@gmail.com',
+      url='https://github.com/heytitle/thesis-designing-recurrent-neural-networks-for-explainability',
+      packages=['src', 'tests'],
+      )
