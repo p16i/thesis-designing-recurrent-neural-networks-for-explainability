5a312042b3152ef8a6d5dbf283484038e3cce707
heytitle <pat.chormai@gmail.com>
2017-11-05 16:04:49 +0100

    correct project path

--- tests/lrp.py
+++ tests/lrp.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
 import tensorflow as tf
+import os
 
 from utils import data_provider
 from model import provider
@@ -10,17 +11,23 @@ logging.disable(logging.DEBUG)
 
 NO_TESTING_DATA = 5
 
+PROJECT_ROOT = '/'.join(os.path.abspath(__file__).split('/')[:-2])
+
+
+def prepend_project_root(path):
+    return '%s/%s' % (PROJECT_ROOT, path)
+
 
 class TestLRP(unittest.TestCase):
     def test_s2(self):
-        TestLRP._test_lrp('../tests/data/s2-network')
+        TestLRP._test_lrp(prepend_project_root('tests/data/s2-network'))
 
     def test_s3(self):
-        TestLRP._test_lrp('../tests/data/s3-network')
+        TestLRP._test_lrp(prepend_project_root('tests/data/s3-network'))
 
     @staticmethod
     def _test_lrp(model):
-        data = data_provider.MNISTData(dir_path='../data/mnist')
+        data = data_provider.MNISTData(dir_path= prepend_project_root('/data/mnist'))
         model = provider.load(model)
 
         model.lrp(data.test2d.x[:NO_TESTING_DATA, :, :], debug=True)
