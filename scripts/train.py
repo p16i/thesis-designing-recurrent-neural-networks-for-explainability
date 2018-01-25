629daa11d80b6a7174a624ae9984111cfbdcb8f3
heytitle <pat.chormai@gmail.com>
2018-01-25 12:32:40 +0100

    s13 : update new model

--- scripts/train.py
+++ scripts/train.py
@@ -5,7 +5,7 @@ import fire
 import numpy as np
 import tensorflow as tf
 
-from model import s2_network, s3_network, deep_4l_network, convdeep_4l_network
+from model import s2_network, s3_network, deep_4l_network, convdeep_4l_network, convdeep_4l_artificial_output
 from utils import logging as lg
 from utils import data_provider, experiment_artifact, network_architecture
 
@@ -15,7 +15,8 @@ NETWORKS = {
     's2_network': s2_network,
     's3_network': s3_network,
     'deep_4l_network': deep_4l_network,
-    'convdeep_4l_network': convdeep_4l_network
+    'convdeep_4l_network': convdeep_4l_network,
+    'convdeep_4l_artificial_output': convdeep_4l_artificial_output
 }
 
 
