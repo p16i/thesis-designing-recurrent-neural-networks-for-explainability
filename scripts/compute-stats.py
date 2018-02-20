9a05cf9f3e47ba00a3e60cd18d945d15e187c732
heytitle <pat.chormai@gmail.com>
2018-02-20 16:26:52 +0100

    s15 : update compute-stat.py script

--- scripts/compute-stats.py
+++ scripts/compute-stats.py
@@ -9,6 +9,7 @@ import numpy as np
 import pickle
 import config
 
+import json
 
 lg.set_logging()
 
@@ -99,39 +100,87 @@ def aopc(dataset, flip_function='minus_one', ref_model='conv-seq1', seqs=[1,4,7]
             pickle.dump(results, output, -1)
 
 
-def relevance_distribution(dataset, seqs=[12], use_sample=False, dry_run=False):
+def relevance_distribution(datasets=[None], models=['s2', 's3', 'deep_4l', 'convdeep_4l'], seqs=[12], use_sample=False,
+                           dry_run=False):
 
-    logging.info('Computing relevance distribution of %s' % dataset)
-    data = data_provider.get_data(dataset)
-    if use_sample:
-        x, y = data.get_samples_for_vis(100)
-    else:
-        x = data.test2d.x
-        y = data.test2d.y
+    # TODO make this configurable
+    original_data_width = 28
+    datasets = datasets.split(',')
+    data_loader = data_provider.DatasetLoader(data_dir='./data')
 
-    results = []
-    methods = list(filter(lambda x: x != 'random', config.METHODS))
 
-    for m in ['s2', 's3', 'deep_4l', 'convdeep_4l']:
-        for s in seqs:
-            model_obj = provider.load(_model_path(m, dataset, s))
-            for e in methods:
-                avg_rel, std_rel = heatmap_evaluation.relevance_distributions(model_obj, x, y, method=e)
+    for dataset in datasets:
+        logging.info('Computing relevance distribution of %s' % dataset)
+        data = data_loader.load(dataset)
+        if use_sample:
+            x, y, digit_mark = data.test2d.x[:10, :, :], data.test2d.y[:10, :], data.test2d_correct_digit_mark[:10, :]
+        else:
+            x = data.test2d.x
+            y = data.test2d.y
+            digit_mark = data.test2d_correct_digit_mark
 
-                results.append(dict(
-                    architecture=m,
-                    seq=s,
-                    method=e,
-                    dataset=dataset,
-                    rel_dist=avg_rel,
-                    std=std_rel
-                ))
+        methods = list(filter(lambda x: x != 'random', config.METHODS))
 
-    if not dry_run:
-        output = './stats/rel-dist-%s.pkl' % (dataset)
-        with open(output, 'wb') as output:
-            pickle.dump(results, output, -1)
+        total_digits = int(x.shape[2]/original_data_width)
 
+        for m in models:
+            results = []
+            for s in seqs:
+
+                steps_per_digit = int((original_data_width / x.shape[2])*s)
+                logging.info('>>>>>>> steps per digit %d' % steps_per_digit)
+
+                model_obj = provider.load(_model_path(m, dataset, s))
+                for e in methods:
+                    avg_rel, std_rel, raw_relevance = heatmap_evaluation.relevance_distributions(model_obj, x, y, method=e)
+
+                    total_relevance = np.sum(raw_relevance, axis=1).reshape(-1, 1)
+                    rel_dist = raw_relevance / (total_relevance + 1e-20)
+
+                    rel_dist_for_digits = np.zeros((raw_relevance.shape[0], total_digits))
+
+                    # aggregate to per digit-level
+                    for i in range(rel_dist_for_digits.shape[1]):
+                        st = i * steps_per_digit
+                        sp = (i+1)*steps_per_digit
+                        print('digit %d : from t=[%d, %d)' %(i, st, sp))
+                        rel_dist_for_digits[:, i] = np.sum(rel_dist[:, st:sp], axis=1)
+
+                    relevance_of_correct_digits = rel_dist_for_digits * digit_mark
+
+                    total_relevance_of_correct_digits = np.sum(relevance_of_correct_digits, axis=1)
+                    avg_std_total_dist = np.mean(np.std(relevance_of_correct_digits, axis=1))
+
+                    max_ss = np.max(relevance_of_correct_digits, axis=1).reshape(-1,1)
+                    percentage_relevance_to_max = relevance_of_correct_digits / (max_ss+1e-10)
+                    avg_percentage_relevance_to_max = np.mean(np.sum(percentage_relevance_to_max * (relevance_of_correct_digits < max_ss), axis=1))
+                    rel_dist_in_data_region = np.mean(total_relevance_of_correct_digits, axis=0)
+                    if use_sample:
+                        print('before multiplying mark %s' % e)
+                        print(rel_dist_for_digits)
+                        print('--------')
+                        print('after marking')
+                        print(relevance_of_correct_digits)
+                        print('Mean dist %f +/- std %f' % (rel_dist_in_data_region, avg_std_total_dist))
+                        print('=================')
+
+                    res = dict(
+                        architecture=m,
+                        seq=s,
+                        method=e,
+                        dataset=dataset,
+                        rel_dist=avg_rel,
+                        std=std_rel,
+                        raw_relevance=raw_relevance,
+                        rel_dist_in_data_region=rel_dist_in_data_region,
+                        avg_std_total_dist=avg_std_total_dist,
+                        avg_percentage_relevance_to_max=avg_percentage_relevance_to_max
+                    )
+                    results.append(res)
+
+                if not dry_run:
+                    with open('./stats/rel-dist-%s-seq-%d-%s.pkl' % (dataset, s, m), 'wb') as fp:
+                        pickle.dump(results, fp)
 
 def _model_path(network, dataset, seq):
     return './final-models/%s_network-%s-seq-%d' % (network, dataset, seq)
