994ae297895a3123f875f3a990c110fb0e091f59
heytitle <pat.chormai@gmail.com>
2018-03-03 21:06:42 +0100

    s15 : Leila`s lrp

--- src/model/architectures/lstm.py
+++ src/model/architectures/lstm.py
@@ -19,6 +19,97 @@ def load(model_path):
     return Network.load(model_path)
 
 
+# from https://github.com/ArrasL/LRP_for_LSTM/blob/master/code/LSTM/LRP_linear_layer.py
+def lrp_linear(hin, w, b, hout, Rout, eps=0.0001, bias_factor=1.0, debug=False):
+    """
+    LRP for a linear layer with input dim D and output dim M.
+    Args:
+    - hin:            forward pass input, of shape (D,)
+    - w:              connection weights, of shape (D, M)
+    - b:              biases, of shape (M,)
+    - hout:           forward pass output, of shape (M,) (unequal to np.dot(w.T,hin)+b if more than one incoming layer!)
+    - Rout:           relevance at layer output, of shape (M,)
+    - bias_nb_units:  number of lower-layer units onto which the bias/stabilizer contribution is redistributed
+    - eps:            stabilizer (small positive number)
+    - bias_factor:    for global relevance conservation set to 1.0, otherwise 0.0 to ignore bias redistribution
+    Returns:
+    - Rin:            relevance at layer input, of shape (D,)
+    """
+    sign_out = tf.where(hout >= 0, tf.ones(tf.shape(hout)), -tf.ones(tf.shape(hout)))  # shape (1, M)
+    # print('------------')
+    # print('singout_', sign_out.get_shape())
+
+
+    Rj_normed = Rout / (hout + eps*sign_out)
+    # print('Rj_normed', Rj_normed.get_shape())
+
+    bias_nb_units = w.get_shape()[0].value
+
+    Rj_bias_term = (eps*sign_out + bias_factor*b)/bias_nb_units*1.
+    # Rj_bias_term = tf.ones(tf.shape(tf.transpose(w))) * tf.transpose(tf)
+    # print('Rj_bias', Rj_bias_term.get_shape())
+
+
+    ww = tf.matmul(Rj_normed, tf.transpose(w))
+    # print('ww', ww.get_shape())
+
+    bb = tf.matmul(Rj_bias_term * Rj_normed, tf.ones(tf.shape(tf.transpose(w))))
+    # print('bb', bb.get_shape())
+
+    # print('hin', hin.get_shape())
+
+    message = hin * ww + bb
+    # print('message', message.get_shape())
+    # aw = hin * tf.multiply(w, tf.transpose(hin)) # 324x128
+    # print('aw')
+    # print(aw.get_shape())
+    #
+    # message = tf.matmul(Rj_normed, tf.transpose(aw + Rj_bias_term))
+    # print('message')
+    # print(message.get_shape())
+
+    return message
+
+
+
+
+    # # print('signout')
+    # # print(sign_out.get_shape())
+    # print('hin')
+    # print(hin.get_shape())
+    #
+    # # print('hinw')
+    # # hinw = tf.expand_dims(tf.multiply(hin, w), 0)
+    # # print(hinw.get_shape())
+    #
+    # numer = tf.expand_dims(tf.multiply(w, tf.transpose(hin)),0) + (
+    #     (bias_factor * b * 1. + eps * sign_out * 1.) * 1. / bias_nb_units)  # shape (D, M)
+    # # numer =
+    # # print('numer')
+    # # print(numer.get_shape())
+    #
+    # denom = hout + (eps * sign_out * 1.)  # shape (1, M)
+    # # print('denom')
+    # # print(denom.get_shape())
+    #
+    # message = (numer / denom) * Rout  # shape (D, M)
+    # # print('Rout')
+    # # print(Rout.get_shape())
+    #
+    # print('message')
+    # print(message.get_shape())
+    # Rin = tf.reduce_sum(message, axis=2)
+    # print('Rin')
+    # print(Rin.get_shape())
+    # #
+    # # # Note: local  layer   relevance conservation if bias_factor==1.0 and bias_nb_units==D
+    # # #       global network relevance conservation if bias_factor==1.0 (can be used for sanity check)
+    # # if debug:
+    # #     print("local diff: ", Rout.sum() - Rin.sum())
+
+    # return Rin
+
+
 class Dag(base.BaseDag):
     def __init__(self, no_input_cols, dims, max_seq_length, architecture: Architecture, optimizer, no_classes):
         super(Dag, self).__init__(architecture, dims, max_seq_length, optimizer=optimizer, no_classes=no_classes)
@@ -53,13 +144,15 @@ class Dag(base.BaseDag):
             print('No. of variables %d' % self.no_variables())
 
             self.activation_labels = ['input_gate', 'forget_gate', 'output_gate', 'xh',
-                                      'new_cell_state', 'output']
+                                      'input_cell_state', 'new_cell_state', 'output']
 
             self.activations = namedtuple('Activations', self.activation_labels) \
                 (**dict([(k, []) for k in self.activation_labels]))
 
             ct = tf.zeros([tf.shape(self.x)[0], architecture.size])
             ht = tf.zeros([tf.shape(self.x)[0], architecture.size])
+
+            self.activations.new_cell_state.append(ct)
 
             # define  dag
             for i in range(0, max_seq_length, no_input_cols):
@@ -81,6 +174,7 @@ class Dag(base.BaseDag):
                 self.activations.output_gate.append(og)
 
                 new_c = tf.tanh(tf.matmul(xh, ly_new_cell_state.W) + ly_new_cell_state.b)
+                self.activations.input_cell_state.append(new_c)
                 new_c_do = tf.nn.dropout(new_c, keep_prob=self.keep_prob)
 
                 ct = ct*fg_do + new_c_do*ig_do
@@ -89,10 +183,6 @@ class Dag(base.BaseDag):
 
                 ht = og_do*tf.tanh(ct_do)
                 self.activations.output.append(ht)
-
-                # self.activations.input_to_cell.append(xr)
-                # xr_do = tf.nn.dropout(xr, keep_prob=self.keep_prob)
-                # output, state = lstm(ii, state)
 
             ht_do = tf.nn.dropout(ht, keep_prob=self.keep_prob)
             self.y_pred = tf.matmul(ht_do, ly_final_output.W) + ly_final_output.b
@@ -131,43 +221,65 @@ class Network(base.BaseNetwork):
 
             rel_to_input = [None]*self._.seq_length
 
+            rx = np.zeros((x.shape[0], self.architecture.recur))
+            total_relevance_reduced = tf.reduce_sum(self.dag.total_relevance, axis=1)
+
             # NOTE: lwr start here
-            rel_from_output_to_cell = self.dag.layers['final_output'].rel_z_plus_prop(
+            rel_to_ht = self.dag.layers['final_output'].rel_z_plus_prop(
                 self.dag.activations.output[-1],
                 self.dag.total_relevance, beta=beta, alpha=alpha
             )
 
-            proportion_to_ct = self.dag.activations.forget_gate[-1] / \
-                              (self.dag.activations.input_gate[-1] + self.dag.activations.forget_gate[-1])
 
-            rel_to_ct = proportion_to_ct*rel_from_output_to_cell
+            rel_to_ct = rel_to_ht
 
-            rel_to_xh = self.dag.layers['new_cell_state'].rel_z_plus_prop(
-                self.dag.activations.xh[-1],
-                (1-proportion_to_ct)*rel_from_output_to_cell, alpha=alpha, beta=beta
-            )
+            rel_to_g = lrp_linear(self.dag.activations.input_gate[-1] * self.dag.activations.input_cell_state[-1],
+                                  tf.eye(self.architecture.size), tf.zeros(self.architecture.size),
+                                  self.dag.activations.output[-1], rel_to_ct)
+
+
+            rel_to_xh = lrp_linear(self.dag.activations.xh[-1],
+                                   self.dag.layers['new_cell_state'].W, self.dag.layers['new_cell_state'].b,
+                                   self.dag.activations.output[-1], rel_to_g)
 
             rel_to_input[-1] = rel_to_xh[:, :-self.architecture.size]
-            rel_to_h = rel_to_xh[:, -self.architecture.size:]
+            rel_to_ht = rel_to_xh[:, -self.architecture.size:]
 
-            rel_to_ct = rel_to_ct + rel_to_h
+            rel_to_ct = lrp_linear(self.dag.activations.forget_gate[-1]*self.dag.activations.new_cell_state[-2],
+                                   tf.eye(self.architecture.size), tf.zeros(self.architecture.size),
+                                   self.dag.activations.new_cell_state[-1], rel_to_ct)
+
+            rr_x, rr_ht, rr_ct, tt_rr = sess.run([rel_to_input[-1], rel_to_ht, rel_to_ct, total_relevance_reduced],
+                                 feed_dict={self.dag.x: x, self.dag.y_target: y, self.dag.rx: rx, self.dag.keep_prob: 1})
+
+            print(rr_x.shape)
+            print('total relevance', tt_rr)
+            print('rr_x relevance', np.sum(rr_x, axis=1))
+            print('rr_ht relevance', np.sum(rr_ht, axis=1))
+            print('rr_ct relevance', np.sum(rr_ct, axis=1))
+            raise 'Force Exit'
+
+            rel_to_ct = rel_to_ct + rel_to_ht
+
 
 
             for i in range(self._.seq_length - 1)[::-1]:
-                proportion_to_ct = self.dag.activations.forget_gate[i] / \
-                                   (self.dag.activations.input_gate[i] + self.dag.activations.forget_gate[i])
+                rel_to_g = lrp_linear(self.dag.activations.input_gate[i] * self.dag.activations.input_cell_state[i],
+                                      tf.eye(self.architecture.size), tf.zeros(self.architecture.size),
+                                      self.dag.activations.output[i], rel_to_ct)
 
-                rel_to_ct = proportion_to_ct*rel_to_ct
-
-                rel_to_xh = self.dag.layers['new_cell_state'].rel_z_plus_prop(
-                    self.dag.activations.xh[i],
-                    (1-proportion_to_ct)*rel_from_output_to_cell, alpha=alpha, beta=beta
-                )
+                rel_to_xh = lrp_linear(self.dag.activations.xh[i],
+                                       self.dag.layers['new_cell_state'].W, self.dag.layers['new_cell_state'].b,
+                                       self.dag.activations.output[i], rel_to_g)
 
                 rel_to_input[i] = rel_to_xh[:, :-self.architecture.size]
-                rel_to_h = rel_to_xh[:, -self.architecture.size:]
+                rel_to_ht = rel_to_xh[:, -self.architecture.size:]
 
-                rel_to_ct = rel_to_ct + rel_to_h
+                rel_to_ct = lrp_linear(self.dag.activations.forget_gate[i]*self.dag.activations.new_cell_state[i-1],
+                                       tf.eye(self.architecture.size), tf.zeros(self.architecture.size),
+                                       self.dag.activations.new_cell_state[i], rel_to_ct)
+
+                rel_to_ct = rel_to_ct + rel_to_ht
 
             pred, heatmaps = self._build_heatmap(sess, x, y,
                                                  rr_of_pixels=rel_to_input, debug=debug)
