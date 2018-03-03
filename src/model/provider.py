73cdacb8cfaa049cef2840ac1c289cc91998661e
heytitle <pat.chormai@gmail.com>
2018-03-03 10:11:29 +0100

    s15 : update provider

--- src/model/provider.py
+++ src/model/provider.py
@@ -1,7 +1,8 @@
 import logging
 from utils import logging as lg
 from model import s2_network, s3_network, deep_4l_network, convdeep_4l_network,\
-    tutorial_network, shallow_2_levels, deep_v21_network, convdeep_gated, convdeep_with_mark, lstm, deep_with_sparsity
+    tutorial_network, shallow_2_levels, deep_v21_network, convdeep_gated, convdeep_with_mark, lstm, deep_with_sparsity,\
+    shallow_v2
 from utils import experiment_artifact
 
 lg.set_logging()
@@ -17,6 +18,7 @@ MODEL_CLASS = {
     'convdeep_gated_network': convdeep_gated,
     'convdeep_with_mark': convdeep_with_mark,
     'deep_with_sparsity': deep_with_sparsity,
+    'shallow_v2': shallow_v2,
     'lstm': lstm
 }
 
@@ -31,4 +33,4 @@ def load(path):
 
 
 def _model_path(network, dataset, seq):
-    return './final-models/%s_network-%s-seq-%d' % (network, dataset, seq)
+    return './final-models/%s-%s-seq-%d' % (network, dataset, seq)
