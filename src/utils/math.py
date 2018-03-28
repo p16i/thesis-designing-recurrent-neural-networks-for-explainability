a1a5e36ece72ae6e1c577b049e69ab742e78af75
heytitle <pat.chormai@gmail.com>
2018-03-28 16:20:31 +0200

    s16 : handle length zero vector from wrong classifications

--- src/utils/math.py
+++ src/utils/math.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 
-def normalize_vector(x):
-    dist = np.sqrt(np.sum(x * x, axis=1)).reshape(x.shape[0], 1)
-    return x / dist
+def compute_length(x):
+    dist = np.sqrt(np.sum(x * x, axis=1))
+    return dist
 
 
 def cosine_similarity(u, v):
-    normed_u = normalize_vector(u)
-    normed_v = normalize_vector(v)
 
-    return np.sum(normed_u * normed_v, axis=1)
+    dot_prod = np.sum(u * v, axis=1)
+
+    length = compute_length(u) * compute_length(v)
+
+    cosine_sim = dot_prod / length
+    cosine_sim[length == 0] = 0
+
+    return cosine_sim
